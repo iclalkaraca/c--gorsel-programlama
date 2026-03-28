0abee11fee3eee798e004fc47b6ec3a6784ea983
iclal karaca <krciclal@gmail.com>
2026-03-29 01:47:08 +0300

    Add files via upload

---
+++ hesap_makinesi/hesapmakinesi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesap_makinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi1=Convert.ToDouble(textBox1.Text);
+            double sayi2=Convert.ToDouble(textBox2.Text);
+            double sonuc = sayi1 + sayi2;
+            MessageBox.Show("sonuc:" +sonuc);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            double sonuc = sayi1 - sayi2;
+            MessageBox.Show("sonuc:" + sonuc);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            double sonuc = sayi1 * sayi2;
+            MessageBox.Show("sonuc:" + sonuc);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            double sonuc = sayi1 / sayi2;
+            MessageBox.Show("sonuc:" + sonuc);
+        }
+    }
+}
